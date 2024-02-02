55c3fbe7aec0f02faa6a0761fd159ef92e635bc7
Niloz23 <147528969+Niloz23@users.noreply.github.com>
2024-02-02 15:08:41 +0200

    Add files via upload

--- NewsPaper/NEWSPORTAL/urls.py
+++ NewsPaper/NEWSPORTAL/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 # Импортируем созданное нами представление
 from .views import Newslist, NewsDetail
+from .views import PostSearchView
 
 
 urlpatterns = [
 
    path('', Newslist.as_view()),
    path('<int:pk>', NewsDetail.as_view()),
+   path('search/', PostSearchView.as_view()),
 
 ]
--- NewsPaper/NEWSPORTAL/views.py
+++ NewsPaper/NEWSPORTAL/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post,Author,Comment
 from datetime import datetime
-
+from .filters import PostFilter
+from django.views import View
 class Newslist(ListView):
     model = Post
     ordering = 'title'
     template_name = 'default.html'
     context_object_name = 'posts'
+    paginate_by = 10
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,10 +18,32 @@ class Newslist(ListView):
         context['next_sale'] = None
         return context
 
+        # Переопределяем функцию получения списка товаров
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
 class NewsDetail(DetailView):
     model = Post
     template_name =  'default.html'
     context_object_name =  'posts'
+
+class PostSearchView(View):
+    template_name = 'NEWSPORTAL/post_search.html'
+
+    def get(self, request, *args, **kwargs):
+        filter = PostFilter(request.GET, queryset=Post.objects.all())
+        return render(request, self.template_name, {'filter': filter})
+
 
 
 
